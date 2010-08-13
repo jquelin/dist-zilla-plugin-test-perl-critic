ce6c6543e260abb21504b801251a243466454e7e
Stephen R. Scaffidi <stephen@scaffidi.net>
2010-08-13 12:25:43 -0400

    added feature to specify perlcritic config file

--- lib/Dist/Zilla/Plugin/CriticTests.pm
+++ lib/Dist/Zilla/Plugin/CriticTests.pm
@@ -6,11 +6,50 @@ package Dist::Zilla::Plugin::CriticTests;
 # ABSTRACT: tests to check your code against best practices
 
 use Moose;
+use Moose::Util qw( get_all_attribute_values );
+
+# this makes dzil add the sections in __DATA__ as "files"
 extends 'Dist::Zilla::Plugin::InlineFiles';
+
+# and when the time comes, treat them like templates
+with qw(
+    Dist::Zilla::Role::FileMunger
+    Dist::Zilla::Role::TextTemplate
+);
+
+
+has critic_config => (
+    is      => 'ro',
+    isa     => 'Str',
+    default => 'perlcritic.rc',
+);
+
+
+# there's probably a better way to get the list of files
+# that were added by this plugin... patches please??
+my %critic_test_filenames =
+    map { $_ => 1 } __PACKAGE__->merged_section_data_names;
+
+
+sub munge_file {
+    my ($self, $file) = @_;
+
+    return unless exists $critic_test_filenames{ $file->name };
+
+    my $template = $file->content;
+    my $stash    = get_all_attribute_values( $self->meta, $self);
+
+    my $rendered = $self->fill_in_string( $template, $stash );
+    $file->content( $rendered );
+}
+
 
 no Moose;
 __PACKAGE__->meta->make_immutable;
 1;
+=pod
+
+=for Pod::Coverage munge_file
 
 =head1 SYNOPSIS
 
@@ -46,4 +85,5 @@ use English qw(-no_match_vars);
 
 eval "use Test::Perl::Critic";
 plan skip_all => 'Test::Perl::Critic required to criticise code' if $@;
+Test::Perl::Critic->import( -profile => "{{ $critic_config }}" ) if -e {{ $critic_config }};
 all_critic_ok();
